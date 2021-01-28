0c40a4eca0109afcabe20bbd18974f86639b3eaa
jork41989 <jork41989@gmail.com>
2021-01-27 22:15:30 -0500

    added the history bar

--- src/components/UserDash/userDash.jsx
+++ src/components/UserDash/userDash.jsx
@@ -3,6 +3,23 @@ import "./userDash.css"
 
 const UserDash = (state) =>{
 
+  let arrMaker = (arr) => {
+    
+    let output = []
+    arr.forEach(day=>{
+      let color
+      if(day >= 5){
+        color = { backgroundColor: "rgba(0, 128, 0, 0.493)" };
+      } else if ( day < 5 && day >= 3){
+        color = { backgroundColor: "rgba(255, 255, 0, 0.466)" };
+      } else {
+        color = { backgroundColor: "rgba(255, 0, 0, 0.602)" };
+      }
+      output.push(<div style={color} className="dayArrEl">{day}</div>)
+    })
+    return output
+  }
+
   return (
     <div className="userDashMain">
       <p>{state.data.username}</p>
@@ -19,6 +36,7 @@ const UserDash = (state) =>{
               <p>{state.data.week1DayCount}</p>
             </div>
           </div>
+          <div className="dayArr">{arrMaker(state.data.week1Raw)}</div>
         </div>
         <div className="weekDiv">
           <p>Week 2</p>
@@ -32,6 +50,7 @@ const UserDash = (state) =>{
               <p>{state.data.week2DayCount}</p>
             </div>
           </div>
+          <div className="dayArr">{arrMaker(state.data.week2Raw)}</div>
         </div>
       </div>
     </div>
